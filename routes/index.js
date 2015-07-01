7d3be0ba07f928dfec456ffcf02ef2537beebc72
ErickJustiniano <justi007_5@hotmail.com>
2015-06-30 23:29:21 -0500

    Ejercicio obligatorio - modificación 2 - parte 5

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
-router.get('/author', function(req, res) {
+router.get('/author', function(req, res, next) {
   res.render('author', { title: 'Erick Alberto Justiniano Caamal' });
 });
 
