047aed37824d146385f70c2b4b413f8bfe5741dc
ErickJustiniano <justi007_5@hotmail.com>
2015-07-01 14:15:11 -0500

    Despligue en Heroku corregido

--- models/models.js
+++ models/models.js
@@ -34,18 +34,19 @@ var sequelize = new Sequelize(null, null, null,
 
 //Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
 exports.Quiz = Quiz; //Exportar definicion de tabla Quiz
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function(){
+sequelize.sync().then(function(){
 	//success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function(){
+	Quiz.count().then(function(count){
 		if (count === 0) { //La tabla se inicializa solo si está vacía
 			Quiz.create({
 				pregunta: 'Capital de Italia',
 				respuesta: 'Roma'
 			})
-			.success(function(){console.log('Base de datos inicializada')});
+			.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
