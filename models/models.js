829f1d38d8b9c8966dedbd3b2adc181373706493
ErickJustiniano <justi007_5@hotmail.com>
2015-07-29 14:29:43 -0500

    Crear Comentario

--- models/models.js
+++ models/models.js
@@ -27,10 +27,20 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 	}
 	);
 
-//Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+//Importar la definicion de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 
-exports.Quiz = Quiz; //Exportar definicion de tabla Quiz
+//Importar la definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+exports.Quiz = Quiz; 		//Exportar definicion de tabla Quiz
+exports.Comment = Comment;	//Exportar definicion de tabla Comment
+
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function(){
